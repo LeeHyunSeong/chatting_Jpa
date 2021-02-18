977a4550438ed47bbcf7a0fc22da1e6a292bd279
LeeHyunSeong <61196115+LeeHyunSeong@users.noreply.github.com>
2021-02-18 23:29:02 +0900

    채팅방에서 채팅 시작 시 웹 소켓이 중지되는 버그 수정 및 메시지 전송 시 서버에서 클라이언트로 보내는 메시지 구성 요소 수정으로 메시지를 보내도 송신이 안되는 버그 수정

--- src/main/java/com/lhs/chatting/handler/SocketHandler.java
+++ src/main/java/com/lhs/chatting/handler/SocketHandler.java
@@ -5,13 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import com.lhs.chatting.model.Room;
 import com.lhs.chatting.util.JsonUtils;
 
 @Component
@@ -22,12 +22,21 @@ public class SocketHandler extends TextWebSocketHandler {
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String messagePayload = message.getPayload();
-        Room room = JsonUtils.readValue(messagePayload, Room.class)
-                .orElseThrow(() -> new RuntimeException("Can not parse json to room"));
+        Map<String, String> payloadMap = JsonUtils.readValue(messagePayload, new TypeReference<Map<String, String>>() {
+        }).orElseThrow(() -> new RuntimeException("Can not parse json to room"));
 
-        List<WebSocketSession> roomSessions = roomSessionsMap.get(room.getNumber());
+        int roomNumber = Integer.parseInt(payloadMap.get("roomNumber"));
+
+        String msg = "{";
+    	for(Map.Entry<String, String> entry : payloadMap.entrySet()) {
+    		msg += "\"" + entry.getKey() + "\":\"" + payloadMap.get(entry.getKey()) + "\",";
+    	}
+    	msg = msg.substring(0, msg.length()-1);
+    	msg += "}";
+    	
+        List<WebSocketSession> roomSessions = roomSessionsMap.get(roomNumber);
         for (WebSocketSession roomSession : roomSessions) {
-            roomSession.sendMessage(new TextMessage(room.toString()));
+        	roomSession.sendMessage(new TextMessage(msg));
         }
     }
 
