93047a9eb0d15f6164a8d5baf4ebb9bdac0d4833
LeeHyunSeong <61196115+LeeHyunSeong@users.noreply.github.com>
2021-03-03 01:46:23 +0900

    이전 커밋으로 인해 오류 내용 일시적 주석처리

--- src/main/java/com/lhs/chatting/config/WebSocketConfig.java
+++ src/main/java/com/lhs/chatting/config/WebSocketConfig.java
@@ -2,22 +2,28 @@ package com.lhs.chatting.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 import com.lhs.chatting.handler.SocketHandler;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Autowired
     SocketHandler socketHandler;
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/chatting/{roomNumber}");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+    	config.enableSimpleBroker("/topic/");
+    	config.setApplicationDestinationPrefixes("/app");
     }
-
+    
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    	registry.addEndpoint("/websockethandler").withSockJS();
+    }
 }
--- src/main/java/com/lhs/chatting/handler/SocketHandler.java
+++ src/main/java/com/lhs/chatting/handler/SocketHandler.java
@@ -1,41 +1,41 @@
 package com.lhs.chatting.handler;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.lhs.chatting.service.WebSocketService;
-import com.lhs.chatting.util.JsonUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import java.util.Map;
-
-@Component
-@RequiredArgsConstructor
-public class SocketHandler extends TextWebSocketHandler {
-    private final WebSocketService webSocketService;
-
-    @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) {
-        Map<String, String> payloadMap = JsonUtils.readValue(message.getPayload(), new TypeReference<Map<String, String>>() {
-        }).orElseThrow(() -> new RuntimeException("Fail to parse json to map"));
-        int roomNumber = Integer.parseInt(payloadMap.get("roomNumber"));
-        String contents = payloadMap.get("contents");
-
-        webSocketService.sendMessage(session, roomNumber, contents);
-    }
-
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        super.afterConnectionEstablished(session);
-        webSocketService.connectToRoom(session);
-    }
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        webSocketService.exitFromRoom(session);
-        super.afterConnectionClosed(session, status);
-    }
-}
+//import com.fasterxml.jackson.core.type.TypeReference;
+//import com.lhs.chatting.service.WebSocketService;
+//import com.lhs.chatting.util.JsonUtils;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.CloseStatus;
+//import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketSession;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+//
+//import java.util.Map;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class SocketHandler extends TextWebSocketHandler {
+//    private final WebSocketService webSocketService;
+//
+//    @Override
+//    public void handleTextMessage(WebSocketSession session, TextMessage message) {
+//        Map<String, String> payloadMap = JsonUtils.readValue(message.getPayload(), new TypeReference<Map<String, String>>() {
+//        }).orElseThrow(() -> new RuntimeException("Fail to parse json to map"));
+//        int roomNumber = Integer.parseInt(payloadMap.get("roomNumber"));
+//        String contents = payloadMap.get("contents");
+//
+//        webSocketService.sendMessage(session, roomNumber, contents);
+//    }
+//
+//    @Override
+//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//        super.afterConnectionEstablished(session);
+//        webSocketService.connectToRoom(session);
+//    }
+//
+//    @Override
+//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+//        webSocketService.exitFromRoom(session);
+//        super.afterConnectionClosed(session, status);
+//    }
+//}
