59136c2ff2896e5a84d2a1dcca02e3922887a345
LeeHyunSeong <61196115+LeeHyunSeong@users.noreply.github.com>
2021-03-08 22:37:40 +0900

    Repository 클래스에 @Repository 어노테이션 추가

--- src/main/java/com/lhs/chatting/repository/RoomRepository.java
+++ src/main/java/com/lhs/chatting/repository/RoomRepository.java
@@ -1,8 +1,10 @@
 package com.lhs.chatting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lhs.chatting.entity.Room;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 }
--- src/main/java/com/lhs/chatting/repository/UserRepository.java
+++ src/main/java/com/lhs/chatting/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.lhs.chatting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lhs.chatting.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
