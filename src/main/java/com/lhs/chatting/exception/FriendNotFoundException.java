e058dea26a766eb5d7cb67bb3d089e7f8d575ed5
LeeHyunSeong <61196115+LeeHyunSeong@users.noreply.github.com>
2021-05-10 03:27:17 +0900

    FriendNotFoundExceptior 생성자 내 format 형식 수정

--- src/main/java/com/lhs/chatting/exception/FriendNotFoundException.java
+++ src/main/java/com/lhs/chatting/exception/FriendNotFoundException.java
@@ -4,6 +4,6 @@ import com.lhs.chatting.model.entity.Friend;
 
 public class FriendNotFoundException extends NotFoundException {
     public FriendNotFoundException(Long friendId) {
-        super(Friend.class, String.format("Id = %s", friendId));
+        super(Friend.class, String.format("Id = %d", friendId));
     }
 }
