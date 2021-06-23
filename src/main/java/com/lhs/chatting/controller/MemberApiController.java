5cf1770e6a6722ab40792446e513b5124e15f23d
LeeHyunSeong <61196115+LeeHyunSeong@users.noreply.github.com>
2021-06-24 00:41:51 +0900

    한 유저의 Members를 호출하는 기능의 Controller 위치 수정

--- src/main/java/com/lhs/chatting/controller/MemberApiController.java
+++ src/main/java/com/lhs/chatting/controller/MemberApiController.java
@@ -1,10 +1,7 @@
 package com.lhs.chatting.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lhs.chatting.model.GetAllMemberResponse;
 import com.lhs.chatting.model.InviteUserRequest;
-import com.lhs.chatting.model.entity.Member;
 import com.lhs.chatting.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -29,15 +24,6 @@ public class MemberApiController {
     public ResponseEntity<Boolean> inviteFriend(@RequestParam InviteUserRequest request) {
         boolean success = memberService.inviteFriend(request.getUserId(), request.getTargetUserId(), request.getRoomId());
         return ResponseEntity.ok(success);
-    }
-
-    @GetMapping
-    public ResponseEntity<GetAllMemberResponse> getMembers(@RequestParam Long userId){
-        List<Member> members = memberService.getMembers(userId);
-        GetAllMemberResponse response = GetAllMemberResponse.builder()
-                .members(members)
-                .build();
-        return ResponseEntity.ok(response);
     }
     
     @PatchMapping("/{memberId}/room-alias")
--- src/main/java/com/lhs/chatting/controller/UserApiController.java
+++ src/main/java/com/lhs/chatting/controller/UserApiController.java
@@ -1,11 +1,24 @@
 package com.lhs.chatting.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lhs.chatting.model.ChangeUserInfoRequest;
+import com.lhs.chatting.model.GetAllMemberResponse;
 import com.lhs.chatting.model.RegisterUserRequest;
+import com.lhs.chatting.model.entity.Member;
 import com.lhs.chatting.model.entity.User;
+import com.lhs.chatting.service.MemberService;
 import com.lhs.chatting.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserApiController {
     private final UserService userService;
+    private final MemberService memberService;
 
     @PostMapping
     public ResponseEntity<Boolean> registerUser(@RequestBody RegisterUserRequest request) {
@@ -34,6 +48,15 @@ public class UserApiController {
         return ResponseEntity.ok(userId);
     }
 
+    @GetMapping("/{userId}/members")
+    public ResponseEntity<GetAllMemberResponse> getMembers(@PathVariable Long userId){
+        List<Member> members = memberService.getMembers(userId);
+        GetAllMemberResponse response = GetAllMemberResponse.builder()
+                .members(members)
+                .build();
+        return ResponseEntity.ok(response);
+    }
+    
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUser(@PathVariable Long userId) {
         User user = userService.getUser(userId);
