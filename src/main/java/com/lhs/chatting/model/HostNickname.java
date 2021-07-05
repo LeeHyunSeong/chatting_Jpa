171f76538cff44beedaf84728424bbdbbb3fcea4
LeeHyunSeong <61196115+LeeHyunSeong@users.noreply.github.com>
2021-07-06 04:22:19 +0900

    유저의 닉네임을 가져오는 CRUD 기능 추가

---
+++ src/main/java/com/lhs/chatting/model/HostNickname.java
@@ -0,0 +1,5 @@
+package com.lhs.chatting.model;
+
+public interface HostNickname {
+    String getNickname();
+}
--- src/main/java/com/lhs/chatting/repository/UserRepository.java
+++ src/main/java/com/lhs/chatting/repository/UserRepository.java
@@ -2,14 +2,18 @@ package com.lhs.chatting.repository;
 
 import java.util.Optional;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.lhs.chatting.model.HostNickname;
 import com.lhs.chatting.model.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String Email);
+
+    Optional<HostNickname> findNicknameById(@Param("userId") Long userId);
 
 }
